12590d30a90c3693d9778134b992b64671ab86a3
Brandon Hackney <brandon@Brandons-MacBook-Pro.local>
2023-10-15 01:06:12 -0700

    Implement lowpass and bandpass filtering

--- Signal Filtering/filterComplexSignal.m
+++ Signal Filtering/filterComplexSignal.m
@@ -1,4 +1,4 @@
-function filterComplexSignal(y,x,filterType, sr)
+function filterComplexSignal(y,x, sr, filterType, varargin)
 % Parse inputs
 filterList = {'lowpass','highpass','bandpass'};
 assert(numel(y) > 1 && numel(x) > 1, 'First two inputs must be the wave vectors from complexSignal')
@@ -17,33 +17,53 @@ fqax = linspace(-Nq,Nq,n);
 
 % Define filter
 step2 = step1; % to be modified
+z = ifftshift(fqax);
 switch filterType
     case 'lowpass'
-        % Cutoff freq = 5 Hz
-        % Remove anything above 5 Hz (and below -5 Hz)
-        cutoff = 5; % ??
-        step2(ifftshift(fqax) >= cutoff & ifftshift(fqax) <= -cutoff) = 0;
+        % Get the cutoff frequency
+        % Only keep the things BELOW this value
+        if exist('varargin', 'var')
+            cutoff = varargin{1};
+        else
+            % Set a default of 5 Hz
+            cutoff = 5;
+        end
         % Apply filter
+        step2(abs(z) >= cutoff) = 0;
     case 'highpass'
-        % Cutoff freq = 1 Hz
-        % Remove anything below 1Hz (and above -1 Hz)
-        cutoff = 1;
-        step2(ifftshift(fqax) <= cutoff & ifftshift(fqax) >= -cutoff) = 0;
+        % Get the cutoff frequency
+        % Only keep the things ABOVE this value
+        if exist('varargin', 'var')
+            cutoff = varargin{1};
+        else
+            cutoff = 1;
+        end
+        % Apply filter
+        step2(abs(z) <= cutoff) = 0;
     case 'bandpass'
-        % Low cutoff = 1 Hz
-        cut1 = 1;
-        % High cutoff = 440 Hz
-        cut2 = 440;
-        % Remove anything below the low cutoff or above the high cutoff
-        % And anything above -cut1 or below -cut2
-        % This is kind of complicated...
-        cutoff = [cut1 cut2];
-
+        % Get TWO cutoff frequencies
+        % Only keep the things BETWEEN these values
+        if exist('varargin', 'var')
+            cutoff = varargin{1};
+            assert(length(cutoff) == 2, 'Cutoff must be a two-element vector when using a bandpass filter');
+            cut1 = cutoff(1);
+            cut2 = cutoff(2);
+        else
+            % Low cutoff = 1 Hz
+            cut1 = 1;
+            % High cutoff = 440 Hz
+            cut2 = 440;
+        end
+        % Report the center band when plotting
+        cutoff = mean([cut1, cut2]);
+        % Apply the filter
+        step2(abs(z) <= cut1 & abs(z) >= cut2) = 0;
 end
+% step2 is still in frequency space - convert back to a wave with ifft
 filtered = ifft(step2);
 
+%% Plot result
 ymax = max(max([filtered;y]));
-% Plot result
 figure();
 subplot(3,1,1)
     plot(x,y);
