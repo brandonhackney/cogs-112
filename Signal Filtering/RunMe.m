9d13dc1b7d12ca85be2813eb4a3aa31189f89065
Brandon Hackney <bhackney@uci.edu>
2023-10-31 21:01:30 -0700

    Add launcher for filtering GUI

---
+++ Signal Filtering/RunMe.m
@@ -0,0 +1,6 @@
+% This runs Filtering.mlapp, which opens a GUI
+% It lets you apply different filters to a signal and watch the outcome
+% (if you just double-click the file, it edits it instead of running)
+
+Filtering;
+% (You also have the option of controlling the signal and filter freqs)
